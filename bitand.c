051172c49641d9dcf677b9261897f9f0efbd5f0d
RIYAARORA20 <riyaarora202003@gmail.com>
2023-12-20 11:51:30 +0530

    msg

---
+++ bitand.c
@@ -0,0 +1,44 @@
+/*
+/*  bitAnd - x & y using only ~ and |
+/* Example bitAnd(6, 5) = 4
+Legal ops:  ~ |  
+/*Max ops: 8
+*/
+#include<stdio.h>
+#include<assert.h>
+
+int bitAnd(int x, int y){
+    return ~(~x | ~y);
+}
+int main(){
+    assert(bitAnd(4,5));
+}
+
+// int bitXor(int x, int y){
+//     return ((x&(~y)) | (y&(~x)));
+// }
+
+// int getByte(int x, int n){
+//     return (x>>(n<<3))&255;
+// }
+// int sign(int x){
+//         return (x>>31 & ~0 ) | (!!x);
+// }
+// int logicalShift( int x, int n){
+//     return (x>>n)&(~(~0 << (32+(~n +1))));
+// }
+// int invert(int a, int p, int n){
+//     int mask = ~((~0)<<n) << p;
+//     // printf("%x",a^mask);
+//     return a ^ mask;
+// }
+
+// int conditional( int x, int y, int z){
+//     int mask = !!x;
+//     mask = ~mask + 1;
+//     return (y & mask) | (z & ~mask);
+// }
+
+// int bang(int x) {
+//     return ((x | (~x + 1)) >> 31) + 1;
+// }
---
+++ bitxor.c
@@ -0,0 +1,6 @@
+/*
+/*  bitXor - x ^ y using only ~ and &
+/* Example bitXor(4, 5) = 1
+Legal ops:  ~ &  
+/* Max ops: 14
+*/
